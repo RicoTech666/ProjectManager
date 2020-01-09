2fd0ba3acb12847dc80df73c8ca6006e7c8ba97f
RicoTech666 <zhoujiachenbe@gmail.com>
2020-01-09 23:14:44 +0800

    [Jiachen] feat-2: using AJAX to fetch project data

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,5 @@
-changeStatusColor();
 function changeStatusColor() {
 	let projectStatus = document.getElementsByClassName("status-col");
-	console.log(projectStatus);
 	let activeStatus = "ACTIVE";
 	let pendingStatus = "PENDING";
 	let closedStatus = "CLOSED";
@@ -20,4 +18,36 @@ function changeStatusColor() {
 				break;
 		}
 	});
+}
+
+const root = " http://localhost:3000/projects";
+getProjectData();
+
+function getProjectData() {
+	ajax({
+		url: root,
+		method: "GET",
+		success: function(response) {
+			renderProjectData(response);
+		},
+		fail: function(error) {
+			console.log("request fail!");
+		},
+	});
+}
+
+function renderProjectData(data) {
+	let projectTable = document.getElementsByClassName("project-data")[0];
+	projectTable.innerHTML = data.reduce((dataContent, curData) => {
+		dataContent += `<tr id="${curData.id}">
+    <td>${curData.name}</td>
+    <td class="description-col">${curData.description}</td>
+    <td>${curData.endTime}</td>
+    <td class="status-col">${curData.status}</td>
+    <td></td>
+  </tr>
+  <tr>`;
+		return dataContent;
+	}, "");
+	changeStatusColor();
 }
